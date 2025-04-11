c5d0af3998b2ceb69e98500f94ab72e9b0b8f6ed
Deodatweb <deodatweb@gmail.com>
2025-04-11 03:41:11 +0200

    1er commit

---
+++ wp-content/languages/loco/plugins/woolentor-fr_FR.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'ShopLentor – WooCommerce Builder for Elementor & Gutenberg','report-msgid-bugs-to'=>'','pot-creation-date'=>'2025-02-18 03:46+0000','po-revision-date'=>'2025-04-04 04:13+0000','last-translator'=>'','language-team'=>'Français','language'=>'fr_FR','plural-forms'=>'nplurals=2; plural=n > 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.7.1; wp-6.7.2','x-domain'=>'woolentor','messages'=>[]];
